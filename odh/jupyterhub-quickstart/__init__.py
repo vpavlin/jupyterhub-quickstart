3b10c90c6746539a6bf1924abae62e1a9de2e106
Harshad Reddy Nalla <hnalla@redhat.com>
2020-08-24 13:05:14 -0400

    :truck: include thoth yaml file for kebechet updates
    Signed-off-by: Harshad Reddy Nalla <hnalla@redhat.com>

---
+++ odh/jupyterhub-quickstart/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# jupyterhub-quickstart
+# Copyright(C) 2019, 2020
+
+"""OpenDatahub.io JupyterHub Quickstart."""
+
+
+__name__ = "jupyterhub-quickstart"
+__version__ = "3.5.0"
